69067f70eb440d048e0d1aadf6c3be5431cd6e5e
RonivonMendes <37858127+RonivonMendes@users.noreply.github.com>
2018-04-05 18:36:00 -0300

    Atualizações
    atualizações

--- index.php
+++ index.php
@@ -9,12 +9,33 @@
 		<div class="corpo">
 			<h3>Análise de Risco</h3>
 			<p>Análise de risco para um novo projeto com base na quantidade de dinheiro e pessoas envolvidas no projeto, Insira os valores para análise!</p>
-			<form action="codFuzzy.php" method="post">
-				<p>Informe o capital disponivel para insvestimento em milhões:<input class="direita" type="text" name="capital" size="2" required>.000.000</p>
-				<p>Informe a quantidade de pessoas envolvidas no projeto:<input type="text" name="pessoas" size="2" required></p>
-				<input type="submit" value="Análisar">
-				<input type="reset" value="Limpar">
-			</form>
+				<table>
+					<form action="codFuzzy.php" method="post">
+					<tr>
+						<td style="text-align: right">
+							Informe o capital disponivel para insvestimento em milhões:
+						</td>
+						<td>
+							<input class="direita" type="text" name="capital" size="2" required>.000.000.
+						</td>
+					</tr>
+					<tr>
+						<td class="direita">
+							Informe a quantidade de pessoas envolvidas no projeto:
+						</td>
+						<td>
+							<input class="direita" type="text" name="pessoas" size="2" required>
+						</td>
+					</tr>
+					<tr>
+						<td class="cent" colspan="2">
+							<input type="submit" value="Análisar">
+							<input type="reset" value="Limpar">
+						</td>
+					</tr>
+				</form>
+			</table>
+			
 		</div>
 
 		<center>
@@ -22,6 +43,5 @@
 		<img src='./img/graficoPessoal.png' alt='' width='400' height='264'>
 		<img src='./img/graficoRisco.png' alt='' width='400' height='264'>
 		</center>";
-
 </body>
 </html>
